b185f8099bae905d43e1574ba9218ac064425090
nnikitenko <nnikitenko@aorti.ru>
2024-04-04 14:57:14 +0300

    Рефакторинг, функциональность фильтрации вакансий по заданным критериям перенесена в класс, реализующий хранение вакансий на диске.

--- src/base_vacancy_storage.py
+++ src/base_vacancy_storage.py
@@ -1,9 +1,10 @@
 from abc import ABC, abstractmethod
+from typing import List
 
 from src.vacancy import Vacancy
 
 
-class BaseSaver(ABC):
+class BaseVacancyStorage(ABC):
     """абстрактный класс, который обязывает реализовать методы для добавления вакансий в файл,
      получения данных из файла по указанным критериям и удаления информации о вакансиях. """
 
@@ -13,5 +14,9 @@ class BaseSaver(ABC):
 
     @abstractmethod
     def delete_vacancy(self, vacancy: Vacancy):
+        pass
+
+    @abstractmethod
+    def filter_vacancies(self, filter_words: List[str], salary_range: str, n: int) -> List[Vacancy]:
         pass
 
