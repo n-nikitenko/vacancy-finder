d4019c2526885126ebd7c6789bf6e595b955f022
nnikitenko <ninanineliya@gmail.com>
2024-04-06 11:16:30 +0300

    1. Рефакторинг 2. Добавлен docstring для HeadHunterAPI::get_vacancies 3. fix ошибки, если не сществует директория 'data' 4. fix записи в json: теперь не экранируются русские символы

--- src/base_api.py
+++ src/base_api.py
@@ -1,7 +1,8 @@
 from abc import ABC, abstractmethod
+from typing import List
 
 
 class BaseAPI(ABC):
     @abstractmethod
-    def get_vacancies(self, key_phrase):
+    def get_vacancies(self, key_phrase) -> List[dict]:
         pass
